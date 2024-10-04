401d6ae6dd43a2040708fb8192b37c71c8aa596c
Harikrishnan <harivk1998@gmail.com>
2024-10-04 18:00:21 +0530

    Initial commit

---
+++ E-commerce_frontEnd/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProductCard from "../components/ProductCard";
+
+const Home = () => {
+  return <div className="text-center my-10">
+    <ProductCard />
+  </div>;
+};
+
+export default Home;
